cd2f193a2403a3969a6126d3c6f19f794a423119
Diego Borjas <diegoborjas@gmail.com>
2023-05-15 00:28:20 -0500

    added functionality to create a readme file

--- Develop/index.js
+++ Develop/index.js
@@ -1,18 +1,24 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
+const fs = require('fs')
 
 // TODO: Create an array of questions for user input
 const { questions } = require('./utils/questions') 
 const { generateMarkdown } = require('./utils/generateMarkdown')
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, 'utf-8', function(err) {
+        console.error(err)
+    })
+}
 
 // TODO: Create a function to initialize app
 function askQuestions() {
     inquirer.prompt(questions).then(function(answers) {
         const generatedAswers = generateMarkdown(answers)
         console.log(generatedAswers)
+        writeToFile('README.md', generatedAswers)
     })
 }
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -28,17 +28,36 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return (`# ${data.title}
+  return (`
+  # ${data.title}
 
-  # License badge goes here
   ${renderLicenseBadge(data.license)}
 
   ## Description
   ${data.description}
 
+  ---
+
   ## Table of Contents
 
+  * [Installation](#installation)
+  
+  * [Usage](#usage)
+
+  * [License](#license)
+
+  * [Contributing](#contributing)
+
+  * [Tests](#tests)
+
+  * [Questions](#questions)
+
   ## Installation
+
+  To install necessary dependencies, run the following command:
+  ---
+  ${data.dependencies}
+  ---
 
   ## Usage
 
